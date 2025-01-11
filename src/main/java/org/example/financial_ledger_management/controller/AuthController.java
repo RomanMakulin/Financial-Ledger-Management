2dbcc5f48565df1caa0885cb54730e20799808ed
Roman Makulin <sup.makulin@mail.ru>
2025-01-11 10:12:21 +0300

    Поправил эндпоинт в соответствии с ТЗ (убрал /api и поправил конфиг секьюрити)

--- src/main/java/org/example/financial_ledger_management/controller/AuthController.java
+++ src/main/java/org/example/financial_ledger_management/controller/AuthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
  * Контроллер для аутентификации пользователей.
  */
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 public class AuthController {
 
     /**
