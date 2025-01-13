58a239d12914d56075626ccc84d50d9e8c5f3cea
Roman Makulin <sup.makulin@mail.ru>
2025-01-13 10:37:27 +0300

    Добавил аннотации репозитория

--- src/main/java/org/example/financial_ledger_management/repository/AccountRepository.java
+++ src/main/java/org/example/financial_ledger_management/repository/AccountRepository.java
@@ -1,9 +1,24 @@
 package org.example.financial_ledger_management.repository;
 
 import org.example.financial_ledger_management.model.Account;
+import org.example.financial_ledger_management.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * Репозиторий для работы с счетами
+ */
+@Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
+
+    /**
+     * Найти счет по пользователю и имени
+     * @param user юзер
+     * @param id уникальный идентификатор счета
+     * @return счет
+     */
+    Optional<Account> findAccountByUserAndId(User user, UUID id);
 }
